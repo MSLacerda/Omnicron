db9a438c0381689965d64242a2801f78134e2c29
Matheus Soares <mathe_oare@hotmail.com>
2018-02-11 01:16:55 -0300

    added mini live reload server, and created tag to library, maked app reference and binding to element

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Omnicron
 Light and minimal library to organize js of webpages
+
+## Source Code 
+
+ Cloning
+
+``git clone https://github.com/MSLacerda/Omnicron`` 
+
+ Install dependencies
+
+``cd Omnicron`` 
+
+``npm install`` 
+
+
--- src/js/omni.js
+++ src/js/omni.js
@@ -1,5 +1,33 @@
 'use strict';
 
 (() => {
+
+    var virtualOmni = document.getElementsByTagName('om-cron')
     
+    const evaluateApp = () => {
+        function Omni(cronSettings) {
+            var _this = this;
+            _this.application = new Object();
+            Object.assign(_this.application, cronSettings);
+
+            _this.virtualElements = document.getElementsByTagName('om-cron');
+
+            for (let index = 0; index < _this.virtualElements.length; index++) {
+                const element = _this.virtualElements[index];
+                if (cronSettings.app === element.attributes['om-name'].textContent)
+                    _this.application.run();
+            }
+
+        }
+        return Omni;
+    }
+
+    if (virtualOmni.length > 0) {
+        window.Omnicron = evaluateApp();
+    }
 })()
+
+window.onload = (event) => {
+    // DEFINE TAGS OF LIBRARY
+    var Omni = document.registerElement('om-cron');
+}
